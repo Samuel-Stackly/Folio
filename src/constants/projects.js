e0fde25dc3989be620df96c238484c2949ebc290
Samuel-Stackly <richsam61@gmail.com>
2026-07-09 18:46:20 +0530

    fix: rename

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,4 +1,4 @@
-import p1 from "../assets/images/p1.png";
+import p1 from "../assets/images/P1.png";
 import p2 from "../assets/images/p2.png";
 import p3 from "../assets/images/p3.png";
 import p4 from "../assets/images/p4.png";
